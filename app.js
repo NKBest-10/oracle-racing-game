3207d84d7de18ff46016caed4720b16a242babba
admin00 <admin@localhost>
2026-03-01 15:17:09 +0700

    fixed_import_maps_v4.7

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 console.log("🚀 Oracle Racing 4.0: Script Loading...");
 
 // ==========================================
